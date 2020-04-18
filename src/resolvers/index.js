f6d9b6ed3fe8c5b057bdbb2fcc71e462fa0ec026
quajoduke <quajoduke305@gmail.com>
2020-04-18 19:21:12 +0000

    change resolvers

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -6,12 +6,42 @@ const metricResolver = require('./metric');
 const newsResolver = require('./news');
 const userResolver = require('./users');
 
+const resolvers = {
+  Mutation: {
+    ...authResolver.login,
+    ...authResolver.createUser,
+    ...authResolver.signOut,
+    ...brandResolver.createBrand,
+    ...brandResolver.deleteBrand,
+    ...brandResolver.updateBrand,
+    ...campaignResolver.createCampaign,
+    ...campaignResolver.updateCampaign,
+    ...influencerResolver.createInfluencer,
+    ...influencerResolver.updateInfluencer,
+    ...metricResolver.createMetric,
+    ...metricResolver.updateMetric,
+    ...newsResolver.createNewsMonitor,
+    ...newsResolver.deleteNewsMonitor,
+    ...newsResolver.updateNewsMonitor,
+    ...userResolver.deleteUser,
+    ...userResolver.updateUser,
+  },
+  Query: {
+    ...brandResolver.getBrand,
+    ...brandResolver.getBrands,
+    ...campaignResolver.getCampaign,
+    ...campaignResolver.getCampaigns,
+    ...influencerResolver.getInfluencer,
+    ...influencerResolver.getInfluencers,
+    ...newsResolver.getNewsMonitor,
+    ...newsResolver.getNewsMonitors,
+    ...userResolver.user,
+    ...userResolver.users,
+    ...metricResolver.campaignMetric,
+  },
+};
+
+console.log(resolvers);
 module.exports = {
-  ...brandResolver,
-  ...authResolver,
-  ...campaignResolver,
-  ...influencerResolver,
-  ...metricResolver,
-  ...newsResolver,
-  ...userResolver,
+  ...resolvers,
 };
